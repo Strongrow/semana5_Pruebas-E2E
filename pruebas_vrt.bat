67023de0a0265a9427f7e7dd893915cac0137859
publio.diaz <p.diazp@uniandes.edu.co>
2023-11-16 09:42:19 -0500

    feat: Creacion de script para ejecucion de pruebas consecutivas

--- pruebas_vrt.bat
+++ pruebas_vrt.bat
@@ -1,2 +1,2 @@
 cd C:\Users\diazpd\Documents\MISO\Pruebas_automatizadas\semana5\entrega_semana_5\semana5_Pruebas-E2E\ghost-e2e-cucumber
-npm test -t .\features\create-tag.feature && npm test -t .\features\create-tag_5_73.feature
+npm test -t .\features\create-tag.feature && npm test -t .\features\create-tag_5_73.feature 
