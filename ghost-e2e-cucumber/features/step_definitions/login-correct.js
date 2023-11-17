54408359ace319420c17cdb3c3a00b7177965d21
publio.diaz <p.diazp@uniandes.edu.co>
2023-11-17 11:10:09 -0500

    feat: Integracion de login correcto con resemblejs

---
+++ ghost-e2e-cucumber/features/step_definitions/login-correct.js
@@ -0,0 +1,47 @@
+const { Given, When, Then } = require('cucumber');
+const ghostPage = require('../support/pages/ghost-page');
+const feature_name = "login-correct";
+
+
+Given('Abrir la pagina home de ghost', async function () {
+
+    await testController.navigateTo(ghostPage.ghost.url_5_69());
+    await testController.takeScreenshot({
+        path: feature_name + "/before-1.png",
+        fullPage: true,
+    });
+});
+
+When('Ingreso el usuario correcto {string}', async function (username) {
+    await testController.typeText(ghostPage.ghost.loginUsername(), username);
+    await testController.takeScreenshot({
+        path: feature_name + "/before-2.png",
+        fullPage: true,
+    });
+});
+
+When('Ingreso el password correcto {string}', async function (password) {
+    await testController.typeText(ghostPage.ghost.loginPassword(), password);
+    await testController.takeScreenshot({
+        path: feature_name + "/before-3.png",
+        fullPage: true,
+    });
+});
+
+When('Voy a el boton de Login', async function () {
+    await testController.click(ghostPage.ghost.loginButton());
+    await testController.takeScreenshot({
+        path: feature_name + "/before-4.png",
+        fullPage: true,
+    });
+});
+
+Then('Verificamos que este el menu post presente', async function () {
+
+    const verificaMenuPost = await ghostPage.ghost.menuPost().exists;
+
+    await testController.expect(verificaMenuPost).ok();
+
+
+});
+
