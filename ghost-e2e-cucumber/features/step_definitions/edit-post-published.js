9557fa3a3309d61db5c3561f003b00488e912454
publio.diaz <p.diazp@uniandes.edu.co>
2023-11-17 10:41:56 -0500

    feat: Se integra la edicion de post publicado con resemblejs

--- ghost-e2e-cucumber/features/step_definitions/edit-post-published.js
+++ ghost-e2e-cucumber/features/step_definitions/edit-post-published.js
@@ -1,53 +1,80 @@
 const { Given, When, Then } = require('cucumber');
 const Role = require('testcafe').Role;
 const ghostPage = require('../support/pages/ghost-page');
-//const Selector = require('testcafe').Selector;
+const feature_name = "edit-post-published";
 
 Given('Home de ghost', async function () {
-    await testController.navigateTo(ghostPage.ghost.url());
-    await testController.takeScreenshot();
+    await testController.navigateTo(ghostPage.ghost.url_5_69());
+    await testController.takeScreenshot({
+        path: feature_name + "/before-1.png",
+        fullPage: true,
+    });
 });
 
 When('Digitar el usuario {string}', async function (username) {
     await testController.typeText(ghostPage.ghost.loginUsername(), username);
-    await testController.takeScreenshot();
+    await testController.takeScreenshot({
+        path: feature_name + "/before-2.png",
+        fullPage: true,
+    });
 });
 
 When('Digitar el password {string}', async function (password) {
     await testController.typeText(ghostPage.ghost.loginPassword(), password);
-    await testController.takeScreenshot();
+    await testController.takeScreenshot({
+        path: feature_name + "/before-3.png",
+        fullPage: true,
+    });
 });
 
 When('Dar en boton Login', async function () {
     await testController.click(ghostPage.ghost.loginButton());
-    await testController.takeScreenshot();
+    await testController.takeScreenshot({
+        path: feature_name + "/before-4.png",
+        fullPage: true,
+    });
 });
 
 
 When('Dar en menu posts', async function () {
     await testController.click(ghostPage.ghost.menuPost());
-    await testController.takeScreenshot();
+    await testController.takeScreenshot({
+        path: feature_name + "/before-5.png",
+        fullPage: true,
+    });
 });
 
 When('Dar en el boton editar', async function () {
     await testController.click(ghostPage.ghost.boton_editar_post_published());
-    await testController.takeScreenshot();
+    await testController.takeScreenshot({
+        path: feature_name + "/before-6.png",
+        fullPage: true,
+    });
 });
 
 When('Editar el titulo del post publicado {string}', async function (titulo) {
     await testController.typeText(ghostPage.ghost.text_area_titulo_post_published(), titulo);
-    await testController.takeScreenshot();
+    await testController.takeScreenshot({
+        path: feature_name + "/before-7.png",
+        fullPage: true,
+    });
 });
 
 
 When('Dar en boton update', async function () {
     await testController.click(ghostPage.ghost.boton_update_post_published());
-    await testController.takeScreenshot();
+    await testController.takeScreenshot({
+        path: feature_name + "/before-8.png",
+        fullPage: true,
+    });
 });
 
 When('Volver a posts', async function () {
     await testController.click(ghostPage.ghost.menuPost());
-    await testController.takeScreenshot();
+    await testController.takeScreenshot({
+        path: feature_name + "/before-9.png",
+        fullPage: true,
+    });
 });
 
 
--- pruebas_vrt.bat
+++ pruebas_vrt.bat
@@ -1,3 +1,4 @@
 cd C:\Users\diazpd\Documents\MISO\Pruebas_automatizadas\semana5\entrega_semana_5\semana5_Pruebas-E2E\ghost-e2e-cucumber
-npm test -t .\features\edit-post-draft.feature && npm test -t .\features\edit-post-draft_5_73.feature && npm test -t .\features\create-tag.feature && npm test -t .\features\create-tag_5_73.feature && npm test -t .\features\cancelar-delete-tag.feature && npm test -t .\features\cancelar-delete-tag_5_73.feature && npm test -t .\features\delete-tag.feature && npm test -t .\features\create-tag.feature && npm test -t .\features\delete-tag_5_73.feature && npm test -t .\features\create-tag.feature
+npm test -t .\features\edit-post-published_5_73.feature && npm test -t .\features\edit-post-draft.feature && npm test -t .\features\edit-post-draft_5_73.feature && npm test -t .\features\create-tag.feature && npm test -t .\features\create-tag_5_73.feature && npm test -t .\features\cancelar-delete-tag.feature && npm test -t .\features\cancelar-delete-tag_5_73.feature && npm test -t .\features\delete-tag.feature && npm test -t .\features\create-tag.feature && npm test -t .\features\delete-tag_5_73.feature && npm test -t .\features\create-tag.feature
+
 
