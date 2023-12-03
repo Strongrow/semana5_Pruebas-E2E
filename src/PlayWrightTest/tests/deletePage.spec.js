ebd8d22c9df43bbb8732c1dc1e08f63189f10205
Lucas Blandón P <l.blandon@uniandes.edu.co>
2023-12-02 21:59:57 -0500

    Escenario de Wright

---
+++ src/PlayWrightTest/tests/deletePage.spec.js
@@ -0,0 +1,43 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+const path = require('path');
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://35.244.81.193/ghost');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Sign In - Pruebas Automatizadas/);
+  
+  //find input de user
+  await page.locator("id=identification").click();
+  await page.fill("id=identification", 'user@example.com');
+  
+  //find input de password
+  await page.locator("id=password").click();
+  await page.fill("id=password", 'Admin12345');
+
+  //find submit button and click
+  await page.getByRole('button', {name: 'Sign in →'}).click();
+  
+});
+
+// Test to check if the login is successful and click on Page
+test('has pages', async ({ page }) => {
+  await page.getByRole('link', { name: 'Page' }).click();
+});
+
+// Test to check if button Delete Works
+test('has New page', async ({ page }) => {
+    await page.goto('http://35.244.81.193/ghost/#/pages');
+    await page.getByRole('link', { name: 'New page' }).click();
+    await page.getByPlaceholder('Page title').fill('Pagina de prueba para borrar');
+    await page.getByTitle('Settings').click();
+    await page.screenshot({ path: 'screenshotNewPageDelete.png' });
+    await page.locator(
+        '//*[@id="entry-controls"]/div/div[2]/div/button/span'
+    ).click();
+    await page.screenshot({ path: 'screenshotDeleteButton.png' });
+    page.on('dialog', dialog => console.log(dialog.message()));
+    await page.getByRole('button',  {name: 'Delete', exact: true }).click();
+    await page.screenshot({ path: 'screenshotDeleteButtonConfirm.png' });
+  });
